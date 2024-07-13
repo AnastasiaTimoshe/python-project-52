31e357c6bd26d084005461f4f3dcef065203a820
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-07-13 15:04:40 +0200

    step_2_3

--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to Hexlet Code")
+    return render(request, 'home.html')
