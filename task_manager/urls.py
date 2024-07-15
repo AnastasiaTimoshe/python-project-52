3298cb85c9bbb2b076a099010f664f0770390e91
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-07-15 11:28:27 +0200

    step_2_5_upd file

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,7 +1,17 @@
+from django.urls import path
+from .views import UserListView, UserCreateView, UserUpdateView, UserDeleteView
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', UserListView.as_view(), name='user_list'),
+    path('create/', UserCreateView.as_view(), name='user_create'),
+    path('<int:pk>/update/', UserUpdateView.as_view(), name='user_update'),
+    path('<int:pk>/delete/', UserDeleteView.as_view(), name='user_delete'),
+]
+
+urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('task_manager.urls')),
+    path('users/', include('users.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from . import views
+from django.contrib import admin
+from django.urls import path, include
 
-def home(request):
-    return render(request, 'home.html')
+
+def index(request):
+    return render(request, 'main/index.html', {'message': 'Hello, World!'})
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+]
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+]
