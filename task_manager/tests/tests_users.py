724134b32a2d6b9eb0eff22409453e7558da7c50
AnastasiaTimoshe <a.n.timoshevskaya@gmail.com>
2024-09-03 23:31:51 +0200

    upd_test_user3

--- task_manager/tests/tests_users.py
+++ task_manager/tests/tests_users.py
@@ -20,64 +20,69 @@ class UsersTest(TestCase):
 
     def test_user_create(self):
         url = reverse("user_create")
-        user = {
+        user_data = {
             "first_name": "Mia",
-            "last_name": "Petrov",
+            "last_name": "Petrova",
             "username": "Mia2013",
             'password1': 'mia',
             'password2': 'mia',
         }
-        response = self.client.post(url, user)
+        response = self.client.post(url, user_data)
         self.assertEqual(response.status_code, 302)
-        test_user = Users.objects.last()
-        assert test_user.first_name == "Mia"
+
         self.assertEqual(Users.objects.count(), 6)
-        new_user = Users.objects.get(username="Mia2013")
-        self.assertEqual(new_user.last_name, "Petrov")
+        test_user = Users.objects.last()
+        self.assertEqual(test_user.first_name, "Mia")
+        self.assertEqual(test_user.username, "Mia2013")
+        self.assertEqual(test_user.last_name, "Petrova")
 
     def test_user_update(self):
         user2 = Users.objects.get(pk=2)
-        url = reverse("user_update", args=[user2.pk])
         self.client.force_login(user2)
-        update_user = {
+        url = reverse("user_update", args=[user2.pk])
+
+        update_data = {
             "first_name": user2.first_name,
             "last_name": user2.last_name,
             "username": "Emi2015",
             'password1': 'securepassword',
             'password2': 'securepassword',
         }
-        response = self.client.post(url, update_user)
+        response = self.client.post(url, update_data)
 
-        if response.status_code == 200:
-            print(response.context['form'].errors)
-
-        updated_user = Users.objects.get(pk=user2.pk)
         self.assertEqual(response.status_code, 302)
         self.assertRedirects(response, reverse("users_list"))
+
+        updated_user = Users.objects.get(pk=user2.pk)
         self.assertEqual(updated_user.username, "Emi2015")
 
     def test_user_update_without_permission(self):
         user2 = Users.objects.get(pk=2)
         user3 = Users.objects.get(pk=3)
-        url = reverse("user_update", args=[user3.pk])
-        update_user = {
-            "first_name": user2.first_name,
-            "last_name": user2.last_name,
-            "username": "Emi2015",
-            'password1': user2.password,
-            'password2': user2.password,
-        }
         self.client.force_login(user2)
-        response = self.client.get(url, update_user)
+        url = reverse("user_update", args=[user3.pk])
+
+        update_data = {
+            "first_name": user3.first_name,
+            "last_name": user3.last_name,
+            "username": "Emi2015",
+            'password1': 'securepassword',
+            'password2': 'securepassword',
+        }
+        response = self.client.post(url, update_data)
+
         self.assertRedirects(response, reverse("users_list"))
-        self.assertEqual(user2.username, "author")
+
+        user3.refresh_from_db()
+        self.assertEqual(user3.username, "executor")
 
     def test_user_delete(self):
         self.assertEqual(Users.objects.count(), 5)
-        user5 = Users.objects.get(pk=5)
-        self.client.force_login(user5)
-        url = reverse("user_delete", args=[user5.pk])
+        user4 = Users.objects.get(pk=5)
+        self.client.force_login(user4)
+        url = reverse("user_delete", args=[user4.pk])
         response = self.client.post(url)
+
         self.assertRedirects(response, reverse('users_list'))
         self.assertEqual(Users.objects.count(), 4)
 
@@ -86,11 +91,8 @@ class UsersTest(TestCase):
 
         user4 = Users.objects.get(pk=5)
         user2 = Users.objects.get(pk=2)
-
         self.client.force_login(user2)
-
         url = reverse("user_delete", args=[user4.pk])
-
         response = self.client.post(url)
 
         self.assertRedirects(response, reverse('users_list'))
@@ -99,5 +101,7 @@ class UsersTest(TestCase):
 
         messages = list(response.wsgi_request._messages)
         expected_error_message = "У вас нет прав для удаления другого пользователя."
-        self.assertTrue(any(expected_error_message in str(m) for m in messages),
-                        "Сообщение об ошибке не найдено")
+        self.assertTrue(
+            any(expected_error_message in str(m) for m in messages),
+            "Сообщение об ошибке не найдено"
+        )
